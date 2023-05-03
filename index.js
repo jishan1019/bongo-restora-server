e5f40e91f8dbeeba1977a0b017e70b7faac95e80
Your Name <imranislamjishan200@gmaile.com>
2023-05-04 01:08:14 +0600

    CORS problem solved

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const cors = require("cors")
+
 const app = express()
+app.use(cors())
+const data = require("./data.json")
 const port = 3000
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
 app.get('/allChefData', (req, res) => {
-    res.send('ffff')
+    res.send(data)
 })
 
 
